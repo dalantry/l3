dd41b0fca80647a4a8bbd84ae1338b83156d7ded
Ilya Pisligin <glkz.pisligin@gmail.com>
2021-12-21 21:05:11 +0300

    faeInOut

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,51 @@
 function randomInteger(min, max) {
-    let rand = min + Math.random() * (max + 1 - min);
-    var number = Math.floor(rand);
+  let rand = min + Math.random() * (max + 1 - min);
+  var number = Math.floor(rand);
 
-    return number;
+  return number;
 }
 
-
-function smileChoice (number){
-    var smile = document.querySelector('.title');
-    smile.textContent = smiles[number]['117'];
+function smileChoice(number) {
+  var smile = document.querySelector(".title");
+  smile.textContent = smiles[number]["117"];
 }
 
-function textChoice(number){
-    var foreignText = document.querySelector('.title-md');
-    foreignText.textContent = words[number]['97'];
+function textChoice(number) {
+  var foreignText = document.querySelector(".title-md");
+  foreignText.textContent = words[number]["97"];
 
-    var rusText = document.querySelector('.text');
-    rusText.textContent = `любить по-${words[number]['97__1']}`;
+  var rusText = document.querySelector(".text");
+  rusText.textContent = `любить по-${words[number]["97__1"]}`;
 }
 
-function onLoad(){
-    var numberArray = randomInteger(0, 96);
-    textChoice(numberArray);
-    smileChoice(numberArray);
+function onLoad() {
+  var numberArray = randomInteger(0, 96);
+  textChoice(numberArray);
+  smileChoice(numberArray);
 }
 
 document.addEventListener("DOMContentLoaded", onLoad());
 
 let timerId = setInterval(() => {
-    var numberArrayInterval = randomInteger(0, 96);
-    smileChoice(numberArrayInterval);
-    textChoice(numberArrayInterval);
+  var numberArrayInterval = randomInteger(0, 96);
+  smileChoice(numberArrayInterval);
+  textChoice(numberArrayInterval);
+}, 4000);
+
+let fadeInOut = setInterval(() => {
+  var foreignText = document.querySelector(".title-md");
+  var rusText = document.querySelector(".text");
+  var smile = document.querySelector(".title");
+
+  foreignText.classList.toggle("animation-on");
+  foreignText.classList.toggle("animation-off");
+  rusText.classList.toggle("animation-on");
+  rusText.classList.toggle("animation-off");
+  smile.classList.toggle("animation-on");
+  smile.classList.toggle("animation-off");
 }, 2000);
 
-
-// setTimeout(flyBurger, 5000); 
+// setTimeout(flyBurger, 5000);
 
 // function flyBurger(){
 //     var burger = document.querySelector('.burger');
